412ba677be4eb11fde298f6ed7d55a1a2bbb3330
Dandy.Chen <ro1963855@gmail.com>
2018-06-20 23:27:05 +0800

    finsh navbar layout
    1.add Roboto font
    2.navbar layout

---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-navbar variant="dark" type="dark" class="ao_navbar">
+        <b-navbar-brand tag="h1" class="ao_brand p-0">Shoptime</b-navbar-brand>
+        <ul class="d-flex m-0 p-0 ao_tabs h-100" left>
+            <a :href="tab.href" class="d-flex" v-for="(tab, index) in tabs" v-bind:key="index">
+                <li class="d-flex align-items-center justify-content-center"
+                    :class="{active: activeTab === index}"
+                    @click="activeTab = index">
+                    <span>{{ tab.name }}</span>
+                </li>
+            </a>
+        </ul>
+        <b-nav-text class="ml-auto ao_user">ADMIN</b-nav-text>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+      activeTab: 0,
+      tabs: [
+        {
+          name: 'HOME',
+          href: '#1',
+        },
+        {
+          name: 'ORDERS',
+          href: '#2',
+        },
+        {
+          name: 'PRODUCT',
+          href: '#3',
+        },
+      ],
+    };
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import './assets/stylesheet/build/all.min.css';
 import App from './App';
 import router from './router';
